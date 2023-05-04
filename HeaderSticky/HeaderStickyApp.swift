620d0ee3e6517767615e998b45f4318b878e7963
githubuiux <uiuxista@gmail.com>
2023-05-04 12:24:54 +0300

    Initial Commit

---
+++ HeaderSticky/HeaderStickyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeaderStickyApp.swift
+//  HeaderSticky
+//
+//  Created by Levent on 4.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct HeaderStickyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
